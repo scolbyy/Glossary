e95d710e9130d8e639663ee4fec9244803b9e2c0
Shawn Colby <shawncolby@billericak12.com>
2020-03-11 12:28:35 -0400

    fggffdg

---
+++ src/com/company/DocumentGlossary.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class DocumentGlossary extends ArrayList<GlossaryEntry> {
+    public DocumentGlossary () {
+        super();
+    }
+    public DocumentGlossary (int a) {
+        super(a);
+    }
+}
